4f6e555ac4a76508cf04637eab5b6e051f1f839f
KhanhNu <mynu2204@gmail.com>
2023-06-08 18:47:37 +0700

    day la commit dau tien

---
+++ main.js
@@ -0,0 +1,36 @@
+/**
+ * Khai báo
+ */
+var fullName = 'Khanh NT';
+var age = 32;
+
+/**
+ * Xử lý
+ */
+//  alert(fullName);
+//  alert(age);
+//  console.log(fullName);
+//  console.log(age);
+//  console.warn('sss');
+//  console.error('aaa');
+//  confirm('Xac nhan name');
+//  prompt('Xac nhan name : ',fullName);
+//  setTimeout(function(){
+//      alert('THong bao')
+//  },2000)
+//  setInterval(function(){
+//      console.warn('bao')
+//  },2000)
+
+
+var a = 7
+console.log(a)
+
+var arrayA = [1, 2, 3, 4, 5, 6, 7];
+console.log(arrayA.length)
+
+if (arrayA.length == a && arrayA.length > 6) {
+    console.log('bang')
+} else {
+    console.log('khong bang')
+}
